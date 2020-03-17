4dde6dd091d29b8db8494436adc80c765b3cdd6b
Pawel <pawel.pietruszka99@gmail.com>
2020-03-17 09:20:43 +0100

    add: adding untracked files

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
-const {welcome} = require('../controllers/index')
+const { welcome } = require('../controllers/index');
 
-router.get('/', welcome)
+router.get('/', welcome);
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
     name: req.user.name,
     id: req.user._id
-  }));
+  })
+);
 
 module.exports = router;
