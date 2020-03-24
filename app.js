426153cf7317ff4eb16e423174a10a285826606b
Pawel <pawel.pietruszka99@gmail.com>
2020-03-24 13:38:26 +0100

    feat: redux oprimisation

--- app.js
+++ app.js
@@ -5,18 +5,12 @@ const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ port: 9090 });
 const { wssHandler } = require('./signalling-server/signal-ws');
 const cors = require('cors');
+const socketHandler = require('./web-socket/socketHandler')
 
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-const MongoStore = require('connect-mongo')(session)
-
-const {
-  addUser,
-  removeUser,
-  getUser,
-  getUsersInRoom
-} = require('./util/users');
+const MongoStore = require('connect-mongo')(session);
 
 const PORT = process.env.PORT || 4000;
 
@@ -39,20 +33,22 @@ mongoose
   .catch(e => console.log(e));
 
 //CORS
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 //Bodyparser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Express Session
-app.use(session({
-  secret: 'mysecret',
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  resave: false,
-  saveUninitialized: false,
-  // cookie: {maxAge: 60000}
-}));
+app.use(
+  session({
+    secret: 'mysecret',
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave: false,
+    saveUninitialized: false
+    // cookie: {maxAge: 60000}
+  })
+);
 
 //Passport middleware
 app.use(passport.initialize());
@@ -60,62 +56,11 @@ app.use(passport.session());
 
 wss.on('connection', wssHandler);
 
-let loggedInUsers = [];
-
-io.on('connection', socket => {
-
-  socket.on('login', (_id, callback) => {
-    if (!loggedInUsers.includes(_id)) {
-      loggedInUsers.push(_id)
-    }
-    callback();
-    console.log(loggedInUsers)
-  })
-  socket.on('logout', (_id, callback) => {
-    console.log(loggedInUsers, 'logging out')
-    loggedInUsers = loggedInUsers.filter(userId => userId !== _id);
-    console.log(loggedInUsers)
-    callback();
-  })
-
-  console.log('New connection established');
-  socket.on('join', (name, roomid, _id, callback) => {
-    console.log('joining', roomid);
-    if (roomid) {
-      const { error, user } = addUser(socket.id, roomid, name, _id);
-      if (error) return callback(error);
-      socket.join(user.roomId);
-
-      io.to(user.roomId).emit('roomData', {
-        room: user.roomId,
-        users: getUsersInRoom(user.roomId)
-      });
-    }
-  });
-
-  socket.on('message', (message, callback) => {
-    let user = getUser(socket.id);
-    console.log(user);
-    io.to(user.roomId).emit('message', { _id: user._id, message: message });
-    callback();
-  });
-
-  socket.on('disconnect', () => {
-    console.log('disconnecting');
-    const user = removeUser(socket.id);
-    if (user) {
-      io.to(user.roomId).emit('roomData', {
-        room: user.roomId,
-        users: getUsersInRoom(user.roomId)
-      });
-    }
-  });
-});
-
+io.on('connection', socket => socketHandler(io, socket));
 
 //Routes
 app
   .use('/messages', require('./routes/messages'))
-  .use('/users', require('./routes/users'))
+  .use('/users', require('./routes/users'));
 
 server.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ web-socket/socketHandler.js
@@ -0,0 +1,66 @@
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('../util/users');
+
+let loggedInUsers = new Set();
+
+function socketHandler(io, socket) {
+  socket.on('login', async (_id, callback) => {
+    await loggedInUsers.add(_id);
+    socket.broadcast.emit('updateUsers', [...loggedInUsers]);
+    callback([...loggedInUsers]);
+  });
+  socket.on('logout', async _id => {
+    await loggedInUsers.delete(_id);
+    socket.broadcast.emit('updateUsers', [...loggedInUsers]);
+  });
+
+  console.log('New connection established');
+  socket.on('join', (name, roomid, _id, callback) => {
+    if (roomid) {
+      let user = getUser(socket.id);
+      if (user) {
+        removeUser(socket.id);
+      }
+      ({ user } = addUser(socket.id, roomid, name, _id));
+      socket.join(user.roomId);
+
+      io.to(user.roomId).emit('roomData', {
+        room: user.roomId,
+        users: getUsersInRoom(user.roomId)
+      });
+    }
+    callback();
+  });
+
+  socket.on('message', (message, callback) => {
+    let user = getUser(socket.id);
+    io.to(user.roomId).emit('message', { _id: user._id, message: message });
+    callback();
+  });
+
+  socket.on('changeConnection', () => {
+    console.log('disconnecting');
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.roomId).emit('roomData', {
+        room: user.roomId,
+        users: getUsersInRoom(user.roomId)
+      });
+    }
+  });
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.roomId).emit('roomData', {
+        room: user.roomId,
+        users: getUsersInRoom(user.roomId)
+      });
+    }
+  });
+}
+
+module.exports = socketHandler;
